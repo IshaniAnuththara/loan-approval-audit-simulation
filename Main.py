7d35bb1bad73c1641e5c66fe990d4eff9b873050
IshaniAnuththara <wijesir-ps22350@stu.kln.ac.lk>
2026-02-23 10:49:10 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -26,7 +26,7 @@ audit_df.loc[audit_df['Income'] < 30000, 'AI_Approval_Prob'] *= 0.1
 print("Audit Data Generated!")
 
 
-# --- TASK 1: Detect Data Poisoning ---
+
 
 poisoned_indices = np.where(audit_df['Credit_Score'] > 850)[0]
 
@@ -40,12 +40,11 @@ else:
 
 print("\n" + "="*40 + "\n")
 
-# --- TASK 2: Analyze the Bias ---
 
 low_income = audit_df[audit_df['Income'] < 30000]
 high_income = audit_df[audit_df['Income'] > 50000]
 
-# Calculate the mean approval probability for both
+
 low_income_avg = low_income['AI_Approval_Prob'].mean()
 high_income_avg = high_income['AI_Approval_Prob'].mean()
 
@@ -53,20 +52,19 @@ print("--- AUDIT REPORT: AI BIAS ANALYSIS ---")
 print(f"Avg Approval Prob (Low Income < $30k):  {low_income_avg:.4f}")
 print(f"Avg Approval Prob (High Income > $50k): {high_income_avg:.4f}")
 
-# Calculate the gap
+
 gap = high_income_avg / low_income_avg
 print(f"\nDisparity Ratio: {gap:.2f}x")
 print(f"Conclusion: High-income earners are {gap:.2f} times more likely to be approved.")
 
-# 1. Identify Poisoned Rows (Credit Score > 850)
+
 poisoned_indices = np.where(audit_df['Credit_Score'] > 850)[0]
 poisoned_rows = audit_df.iloc[poisoned_indices]
 
-# 2. Identify "Bias Victims"
 
 bias_victims = audit_df[(audit_df['Credit_Score'] > 750) & (audit_df['AI_Approval_Prob'] < 0.1)]
 
-# 3. Create the "Red Flag Report"
+
 red_flags = pd.concat([poisoned_rows, bias_victims]).drop_duplicates()
 
 print(f"--- AUDIT COMPLETE ---")
@@ -75,4 +73,5 @@ print(f"Potential Bias Victims Found: {len(bias_victims)}")
 
 # Export to CSV for the client
 red_flags.to_csv('audit_red_flags.csv', index=False)
+
 print("\n'audit_red_flags.csv' has been generated for the CEO.")
